27161435f9f802605a637d0630245308ee37d7c1
els0212@nate.com <hyi@c1r7s2.42seoul.kr>
2021-02-05 22:37:52 +0900

    final

--- srcs/ssl_setup.sh
+++ srcs/ssl_setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=KR/ST=SeoulL=Seoul/O=42Seoul/OU=hyi/CN=localhost" -keyout private.key -out public.key
-chmod 600 public.key private.key
-#mkdir /etc/ssl
 mv public.key /etc/ssl/
 mv private.key /etc/ssl/
-
